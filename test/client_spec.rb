5b2b946b1ea43921ee59b0508469ce1e3155292b
Todd Eichel <todd@toddeichel.com>
2011-01-27 03:13:39 -0500

    Tests for option command and stack setting.

--- test/client_spec.rb
+++ test/client_spec.rb
@@ -70,73 +70,108 @@ describe "Duostack client" do
         run_command("ps --app #{@app_name}").should match('Instance ID')
       end
       
-      it "should allow adding env vars" do
-        run_command("env add env1=var1 --app #{@app_name}").should match('env1 => var1')
-      end
       
-      it "should allow adding multiple env vars at once" do
-        result = run_command("env add env2=var2 env3=var3 --app #{@app_name}")
-        result.should match('env2 => var2')
-        result.should match('env3 => var3')
-      end
+      describe "option commands" do
       
-      it "should allow adding quoted env vars" do
-        run_command("env add env4='multi word string' --app #{@app_name}").should match('env4 => multi word string')
-      end
+        it "should list options for stack" do
+          result = run_command("option stack --app #{@app_name}")
+          result.should match("autodetect")
+          result.should match("ruby-mri-1.9.2")
+        end
       
-      it "should allow adding env vars containing single/double quotes" do
-        run_command(%Q(env add env5='"double quoted"' --app #{@app_name})).should match(%Q(env5 => "double quoted"))
-        run_command(%Q(env add env6="'single quoted'" --app #{@app_name})).should match(%Q(env6 => 'single quoted'))
-      end
+        it "should allow setting option for stack" do
+          result = run_command("option stack ruby-mri-1.9.2 --app #{@app_name}")
+          result.should match("App will be migrated to ruby-mri-1.9.2 during next Git push.")
+        end
       
-      it "should list env vars" do
-        result = run_command("env --long --app #{@app_name}")
-        result.should match(run_command("env list --app #{@app_name}"))
-        result.should match('env1 => var1')
-        result.should match('env2 => var2')
-        result.should match('env3 => var3')
-        result.should match('env4 => multi word string')
-        result.should match('env5 => "double quoted"')
-        result.should match("env6 => 'single quoted'")
-      end
-      
-      it "should see env vars in console" do
-        run_command("restart --app #{@app_name}") # need to restart first
+        it "should reflect stack change upon re-push" do
+          `cd #{@app_path} && touch poke && git add poke && git ci -m "poke" && git push duostack master 2>&1`
+          result = `expect #{File.dirname(__FILE__)}/console_test.expect #{@app_name} "IO.popen('ruby -v') { |f| puts f.gets }"`.gsub("\r", '')
+          expected = <<-END.gsub(/^ {12}/, '').gsub("\r", '')
+            spawn duostack console --app #{@app_name}
+            Connecting to Ruby console for #{@app_name}...
+            >> IO.popen('ruby -v') { |f| puts f.gets }
+            ruby 1.9.2p136 (2010-12-25 revision 30365) [x86_64-linux]
+            => nil
+            >> exit
+            Connection to duostack.net closed.
+          END
         
-        result = `expect #{File.dirname(__FILE__)}/console_test.expect #{@app_name} "ENV['env1']"`.gsub("\r", '')
-        expected = <<-END.gsub(/^ {10}/, '').gsub("\r", '')
-          spawn duostack console --app #{@app_name}
-          Connecting to Ruby console for #{@app_name}...
-          >> ENV['env1']
-          => "var1"
-          >> exit
-          Connection to duostack.net closed.
-        END
-        
-        result.should == expected
-      end
+          result.should == expected
+        end
       
-      it "should allow removing env vars" do
-        run_command("env remove env1 --app #{@app_name}").should == "Environment variable(s) removed"
-        run_command("env --app #{@app_name}").should_not match('env1')
-      end
+      end      
+      
+      describe "environment variables" do
+      
+        it "should allow adding env vars" do
+          run_command("env add env1=var1 --app #{@app_name}").should match('env1 => var1')
+        end
+      
+        it "should allow adding multiple env vars at once" do
+          result = run_command("env add env2=var2 env3=var3 --app #{@app_name}")
+          result.should match('env2 => var2')
+          result.should match('env3 => var3')
+        end
+      
+        it "should allow adding quoted env vars" do
+          run_command("env add env4='multi word string' --app #{@app_name}").should match('env4 => multi word string')
+        end
+      
+        it "should allow adding env vars containing single/double quotes" do
+          run_command(%Q(env add env5='"double quoted"' --app #{@app_name})).should match(%Q(env5 => "double quoted"))
+          run_command(%Q(env add env6="'single quoted'" --app #{@app_name})).should match(%Q(env6 => 'single quoted'))
+        end
+      
+        it "should list env vars" do
+          result = run_command("env --long --app #{@app_name}")
+          result.should match(run_command("env list --app #{@app_name}"))
+          result.should match('env1 => var1')
+          result.should match('env2 => var2')
+          result.should match('env3 => var3')
+          result.should match('env4 => multi word string')
+          result.should match('env5 => "double quoted"')
+          result.should match("env6 => 'single quoted'")
+        end
+      
+        it "should see env vars in console" do
+          run_command("restart --app #{@app_name}") # need to restart first
+        
+          result = `expect #{File.dirname(__FILE__)}/console_test.expect #{@app_name} "ENV['env1']"`.gsub("\r", '')
+          expected = <<-END.gsub(/^ {12}/, '').gsub("\r", '')
+            spawn duostack console --app #{@app_name}
+            Connecting to Ruby console for #{@app_name}...
+            >> ENV['env1']
+            => "var1"
+            >> exit
+            Connection to duostack.net closed.
+          END
+        
+          result.should == expected
+        end
+      
+        it "should allow removing env vars" do
+          run_command("env remove env1 --app #{@app_name}").should == "Environment variable(s) removed"
+          run_command("env --app #{@app_name}").should_not match('env1')
+        end
 
-      it "should allow removing multiple env vars at once" do
-        run_command("env remove env2 env3 --app #{@app_name}").should == "Environment variable(s) removed"
+        it "should allow removing multiple env vars at once" do
+          run_command("env remove env2 env3 --app #{@app_name}").should == "Environment variable(s) removed"
         
-        list = run_command("env --app #{@app_name}")
-        list.should_not match('env2')
-        list.should_not match('env3')
-      end
+          list = run_command("env --app #{@app_name}")
+          list.should_not match('env2')
+          list.should_not match('env3')
+        end
       
-      it "should require confirmation to clear env vars" do
-        run_command("env clear --app #{@app_name} 2>&1").should match("command requires confirmation")
-      end
+        it "should require confirmation to clear env vars" do
+          run_command("env clear --app #{@app_name} 2>&1").should match("command requires confirmation")
+        end
       
-      it "should clear env vars" do
-        run_command("env clear --app #{@app_name} --confirm").should match("Environment variables cleared")
-        list = run_command("env --app #{@app_name}")
-        list.should be_empty
+        it "should clear env vars" do
+          run_command("env clear --app #{@app_name} --confirm").should match("Environment variables cleared")
+          list = run_command("env --app #{@app_name}")
+          list.should be_empty
+        end
       end
       
       
